229e7679dabcd602cb30b460d113cbbddbd5ba40
SKR_301 <chunnu.bihat@gmail.com>
2020-04-30 22:11:21 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const INITIAL_DIFFICULTY=3;
+const MINE_RATE=1000;
+
+const GENESIS_DATA={
+	timestamp:1,
+	lasthash:'-----',
+	hash:'#1',
+	nonce:0,
+	difficulty:INITIAL_DIFFICULTY,
+	data:[]
+};
+
+const STARTING_BALANCE=1000;
+
+module.exports={GENESIS_DATA,MINE_RATE,STARTING_BALANCE};
---
+++ index.js
@@ -0,0 +1,90 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request');
+const path = require('path');
+const Blockchain = require('./blockchain');
+const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
+
+const app = express();
+const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
+const pubsub=new PubSub({blockchain,transactionPool});
+const DEFAULT_PORT=3000;
+const ROOT_NODE_ADDRESS=`http://localhost:${DEFAULT_PORT}`;
+
+app.use(bodyParser.json());
+
+app.get('/api/blocks',(req,res)=>{
+	res.json(blockchain.chain);
+});
+
+app.post('/api/mine',(req,res)=>{
+	const {data} = req.body;
+
+	blockchain.addBlock({data});
+
+	pubsub.broadcastChain();
+	res.redirect('/api/blocks');
+});
+
+
+app.post('/api/transact',(req,res)=>{
+	const {amount,recipient}=req.body;
+	let transaction=transactionPool.existingTransaction({inputAddress:wallet.publicKey});
+
+	try{	
+		if(transaction){
+			transaction.update({senderWallet:wallet,recipient,amount});
+		}
+		else{
+			transaction=wallet.createTransaction({recipient,amount});
+		}
+	}
+	catch(error){
+		return res.status(400).json({type:'error',message:error.message});
+	}
+
+	transactionPool.setTransaction(transaction);
+	pubsub.broadcastTransaction(transaction);
+	res.json({type:'success',transaction});
+});
+
+app.get('/api/transaction-pool-map',(req,res)=>{
+	res.json(transactionPool.transactionMap);
+});
+
+const syncWithRootState=()=>{
+	request({url:`${ROOT_NODE_ADDRESS}/api/blocks`},(error,response,body)=>{
+		if(!error && response.statusCode===200){
+			const rootChain=JSON.parse(body);
+			console.log('\nReplace chain on sync with:',rootChain);
+			blockchain.replaceChain(rootChain);
+		}
+	});
+
+	request({url:`${ROOT_NODE_ADDRESS}/api/transact`},(error,response,body)=>{
+		if(!error && response.statusCode===200){
+			const rootTransactionPoolMap=JSON.parse(body);
+			console.log('\nReplace map on sync with:',rootTransactionPoolMap);
+			transactionPool.setMap(rootTransactionPoolMap);
+		}
+	});
+};
+
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT==='true'){
+	PEER_PORT=DEFAULT_PORT+Math.ceil(Math.random()*1000);
+}
+
+const PORT=PEER_PORT||DEFAULT_PORT;
+app.listen(PORT,()=>{
+	console.log(`listening at: ${PORT}`)
+
+	if(PORT!==DEFAULT_PORT){	
+		syncWithRootState();
+	}
+});
